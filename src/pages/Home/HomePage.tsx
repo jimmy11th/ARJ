ad32e7beebe004c2fd4c6cfe516d851d1ed1445b
jimmy.chen <jimmy.chen@nirvana-info.com>
2023-08-28 18:13:07 +0800

    add template

--- src/pages/Home/HomePage.tsx
+++ src/pages/Home/HomePage.tsx
@@ -1,19 +1,18 @@
-import Counter from '@/components/Counter/Counter';
-import TemplateTester from '@/components/TemplateTester/TemplateTester';
-import { Container, Stack, Typography } from '@mui/material';
+import { Container, Stack } from '@mui/material';
 
 const HomePage = () => {
   return (
-    <Container sx={{ py: 2, position: 'relative' }}>
-      <Stack gap={1} my={2}>
-        <Typography textAlign="center" variant="h2">
-        </Typography>
-        <Typography textAlign="center" variant="subtitle1">
-          React + TS + Vite + Redux + RTK + MUI + RRD + Prettier
-        </Typography>
+    <Container sx={{ maxWidth:'lg', py: 2, position: 'relative' }}>
+      <Stack gap={0} my={0}>
+       <div style={{height:'35px', backgroundColor:"#570DF8"}}>Announcements</div>
+       <div style={{height:'57px', backgroundColor:"#F000B8"}}>Headers</div>
+       <div style={{height:'320px', backgroundColor:"#37CDBE"}}>products</div>
+       <div style={{height:'420px', backgroundColor:"#3D4451"}}>sales</div>
+       <div style={{height:'460px', backgroundColor:"#FFFFFF"}}>company</div>
+       <div style={{height:'240px', backgroundColor:"#3ABFF8"}}>Items</div>
+       <div style={{height:'650px', backgroundColor:"#36D399"}}>Communications</div>
+       <div style={{height:'320px', backgroundColor:"#FBBD23"}}>Footer</div>
       </Stack>
-      <TemplateTester />
-      <Counter />
     </Container>
   );
 };
