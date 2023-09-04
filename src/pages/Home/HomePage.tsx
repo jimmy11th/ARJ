bfa895ee8ebd0ffb0031bf4a022e47f1aaff402e
jimmy.chen <jimmy.chen@nirvana-info.com>
2023-09-04 13:46:03 +0800

    add banner

--- src/pages/Home/HomePage.tsx
+++ src/pages/Home/HomePage.tsx
@@ -1,11 +1,16 @@
-import { Container, Stack } from '@mui/material';
+import PrimarySearchAppBar from '@/components/AppBar/AppBar';
+import { Box, Stack } from '@mui/material';
 
 const HomePage = () => {
   return (
-    <Container sx={{ maxWidth:'lg', py: 2, position: 'relative' }}>
+    <Box sx={{ maxWidth:'lg' }}>
       <Stack gap={0} my={0}>
-       <div style={{height:'35px', backgroundColor:"#570DF8"}}>Announcements</div>
-       <div style={{height:'57px', backgroundColor:"#F000B8"}}>Headers</div>
+       <Box style={{height:'44px', backgroundColor:"black", cursor: "pointer", fontSize: "14px", display:"flex", justifyContent:"center", alignItems:"center"}}>
+        <span style={{ color: 'white' }}>
+          FREE SHIPPING ON ORDERS OVER 300
+        </span>
+       </Box>
+       <PrimarySearchAppBar />
        <div style={{height:'320px', backgroundColor:"#37CDBE"}}>products</div>
        <div style={{height:'420px', backgroundColor:"#3D4451"}}>sales</div>
        <div style={{height:'460px', backgroundColor:"#FFFFFF"}}>company</div>
@@ -13,7 +18,7 @@ const HomePage = () => {
        <div style={{height:'650px', backgroundColor:"#36D399"}}>Communications</div>
        <div style={{height:'320px', backgroundColor:"#FBBD23"}}>Footer</div>
       </Stack>
-    </Container>
+    </Box>
   );
 };
 
