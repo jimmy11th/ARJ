07a756c76e836b44e35d9b0c7742413eac5a624f
jimmy.chen <jimmy.chen@nirvana-info.com>
2023-10-08 18:45:39 +0800

    ADD height 340px

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,15 +1,21 @@
 import { Box, Button, Typography } from '@mui/material';
 
-const Card = (props: any) => {
-    const {name, price, URL } = props
-    return (
-        <Box sx={{display:'flex', flexDirection:'column', justifyContent:'flex-start',gap:'20px',height:'500px'}}>
-            <img width='100%' src={URL} alt={name}/>
-            <Typography >{name}</Typography>
-            <Typography >$ {price}</Typography>
-            <Button style={{marginTop: "auto"}}>Quick View</Button>
-        </Box>
-    );
+interface ICard {
+    name: string,
+    price: string,
+    URL: string
 }
+
+const Card = (props: ICard) => {
+  const { name, price, URL } = props;
+  return (
+    <Box sx={{ display: 'flex', flexDirection: 'column', justifyContent:"space-between",height:'340px' }}>
+        <img width="100%" src={URL} alt={name} />
+        <Typography>{name}</Typography>
+        <Typography sx={{ margin: '0 auto' }}>$ {price}</Typography>
+        <Button sx={{ margin: '0 auto' }}>Quick View</Button>
+    </Box>
+  );
+};
 
 export default Card;
