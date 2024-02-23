cfa49877af60fc76e94c5661ceb08821a4f40948
jimmy.chen <jimmy.chen@nirvana-info.com>
2024-02-23 16:01:48 +0800

    fix some bug

--- src/pages/Home/HomePage.tsx
+++ src/pages/Home/HomePage.tsx
@@ -4,56 +4,58 @@ import LabTabs from '@/components/Tabs/Tabs';
 import { mockup } from '@/data/data';
 import { Box, Stack, Typography } from '@mui/material';
 import Grid from '@mui/material/Unstable_Grid2';
-import { driver } from "driver.js";
-import "driver.js/dist/driver.css";
+import 'driver.js/dist/driver.css';
+import { Outlet } from 'react-router-dom';
 
 const HomePage = () => {
-  const arr = mockup.productCategory
-  const driverObj = driver();
-  driverObj.highlight({
-    element: "#some-element",
-    popover: {
-      title: "Title",
-      description: "Description"
-    }
-  });
+  const arr = mockup.productCategory;
   return (
-    <Box sx={{ maxWidth:'xxl' }}>
+    <Box sx={{ maxWidth: 'xxl' }}>
       <Stack gap={0} my={0}>
-       {/* <Box style={{height:'44px', backgroundColor:"black", cursor: "pointer", fontSize: "14px", display:"flex", justifyContent:"center", alignItems:"center"}}>
+        {/* <Box style={{height:'44px', backgroundColor:"black", cursor: "pointer", fontSize: "14px", display:"flex", justifyContent:"center", alignItems:"center"}}>
         <span style={{ color: 'white' }}>
           FREE SHIPPING ON ORDERS OVER 300
         </span>
        </Box> */}
-       <ResponsiveAppBar />
-       <Box>
-         <SwipeableTextMobileStepper />
-      </Box>
-      <Box id="some-element" sx={{ maxWidth:'xxl', paddingBottom:'4rem', paddingTop:'4rem', display: 'flex', flexDirection:'column', alignItems:'center'}}>
-        <Typography variant="h1" gutterBottom align="center" width="50%">
-        ALLMAX PROFESSIONAL GRADE SUPPLEMENTS
-        </Typography>
-        <Typography variant="body1" gutterBottom align="center" width="80%">
-        We are a trusted brand for athletes and fitness enthusiasts looking for the highest quality supplements to optimize their workouts and achieve their fitness goals. Our product ingredients are of the purest quality, meet pharmaceutical grade, and are lab-tested.
-        </Typography>
-      </Box>
-      <LabTabs />
-       <Box sx={{ flexGrow: 1, p: 2 }}>
-                <Grid
-                    container
-                    spacing={2}
-                >
-                    {arr.map((category) => (
-                        <Grid key={category.id} {...{ xs: 6, sm: 4, md: 3, lg: 2 }} minHeight={120}>
-                                <img width='100%' src={category.URL}></img>
-                        </Grid>
-                    ))}
-                </Grid>
-            </Box>
-       <div style={{height:'460px', backgroundColor:"#FFFFFF"}}>company</div>
-       <div style={{height:'240px', backgroundColor:"#3ABFF8"}}>Items</div>
-       <div style={{height:'650px', backgroundColor:"#36D399"}}>Communications</div>
-       <div style={{height:'320px', backgroundColor:"#FBBD23"}}>Footer</div>
+        <ResponsiveAppBar />
+        <Box>
+          <SwipeableTextMobileStepper />
+        </Box>
+        <Box
+          id="some-element"
+          sx={{
+            maxWidth: 'xxl',
+            paddingBottom: '4rem',
+            paddingTop: '4rem',
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+          }}
+        >
+          <Typography variant="h1" gutterBottom align="center" width="50%">
+            ALLMAX PROFESSIONAL GRADE SUPPLEMENTS
+          </Typography>
+          <Typography variant="body1" gutterBottom align="center" width="80%">
+            We are a trusted brand for athletes and fitness enthusiasts looking for the highest quality supplements to
+            optimize their workouts and achieve their fitness goals. Our product ingredients are of the purest quality,
+            meet pharmaceutical grade, and are lab-tested.
+          </Typography>
+        </Box>
+        <LabTabs />
+        <Box sx={{ flexGrow: 1, p: 2 }}>
+          <Grid container spacing={2}>
+            {arr.map((category) => (
+              <Grid key={category.id} {...{ xs: 6, sm: 4, md: 3, lg: 2 }} minHeight={120}>
+                <img width="100%" src={category.URL}></img>
+              </Grid>
+            ))}
+          </Grid>
+        </Box>
+        <Outlet />
+        <div style={{ height: '460px', backgroundColor: '#FFFFFF' }}>company</div>
+        <div style={{ height: '240px', backgroundColor: '#3ABFF8' }}>Items</div>
+        <div style={{ height: '650px', backgroundColor: '#36D399' }}>Communications</div>
+        <div style={{ height: '320px', backgroundColor: '#FBBD23' }}>Footer</div>
       </Stack>
     </Box>
   );
--- src/routes/Routing.tsx
+++ src/routes/Routing.tsx
@@ -1,12 +1,18 @@
 import Login from '@/pages/Auth/Login';
 import Register from '@/pages/Auth/Register';
 import HomePage from '@/pages/Home/HomePage';
+import Product from '@/pages/Product';
 import { Route, Routes } from 'react-router-dom';
 
 const Routing = () => {
   return (
     <Routes>
-      <Route path="*" element={<HomePage />} />
+      <Route path="/" element={<HomePage />}>
+        <Route
+          path="product"
+          element={<Product />}
+        />
+      </Route>
       <Route path="/login" element={<Login />} />
       <Route path="/register" element={<Register />} />
     </Routes>
